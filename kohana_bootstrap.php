8fa95b5f567ee1df5e668ecddf58b4ea64ee3327
Mr. Kerk <kerkness@iamthefastest.com>
2009-06-27 15:22:10 +0000

    splitting kohana init into two files, kohana_bootstrap.php and kohana_index.php

---
+++ kohana_bootstrap.php
@@ -0,0 +1,76 @@
+<?php
+
+// Recreate the Kohana index.php process with modifications
+	
+//-- Environment setup --------------------------------------------------------
+
+/**
+ * Set the default time zone.
+ *
+ * @see  http://docs.kohanaphp.com/features/localization#time
+ * @see  http://php.net/timezones
+ */
+date_default_timezone_set(get_option('kohana_default_time_zone'));
+
+/**
+ * Enable the Kohana auto-loader.
+ *
+ * @see  http://docs.kohanaphp.com/features/autoloading
+ * @see  http://php.net/spl_autoload_register
+ */
+spl_autoload_register(array('Kohana', 'auto_load'));
+
+/**
+ * Enable Kohana exception handling, adds stack traces and error source.
+ *
+ * @see  http://docs.kohanaphp.com/features/exceptions
+ * @see  http://php.net/set_exception_handler
+ */
+set_exception_handler(array('Kohana', 'exception_handler'));
+
+/**
+ * Enable Kohana error handling, converts all PHP errors to exceptions.
+ *
+ * @see  http://docs.kohanaphp.com/features/exceptions
+ * @see  http://php.net/set_error_handler
+ */
+set_error_handler(array('Kohana', 'error_handler'));
+
+//-- Kohana configuration -----------------------------------------------------
+
+/**
+ * Initialize Kohana, setting the default options.
+ *
+ * The following options are available:
+ * - base_url:   path, and optionally domain, of your application
+ * - index_file: name of your index file, usually "index.php"
+ * - charset:    internal character set used for input and output
+ * - profile:    enable or disable internal profiling
+ * - caching:    enable or disable internal caching
+ */
+Kohana::init(array('charset' => 'utf-8', 'base_url' => get_option('kohana_base_url') ));
+
+/**		**** Enable modules as defined in plugin settings
+ * Enable modules. Modules are referenced by a relative or absolute path.
+ */
+$k_mods = explode(',', get_option('kohana_modules') );
+foreach( $k_mods as $km ){
+	$mods[trim($km)] = MODPATH.trim($km);
+}
+Kohana::modules($mods);
+
+/**
+ * Attach the file write to logging. Any Kohana_Log object can be attached,
+ * and multiple writers are supported.
+ */
+Kohana::$log->attach(new Kohana_Log_File(APPPATH.'logs'));
+
+/**
+ * Set the routes. Each route must have a minimum of a name, a URI and a set of
+ * defaults for the URI.
+ */
+Route::set('default', '(<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'controller' => get_option('kohana_default_controller'),
+		'action' => get_option('kohana_default_action'),
+		'id' => get_option('kohana_default_id')));	
---
+++ kohana_index.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * The directory in which your application specific resources are located.
+ * The application directory must contain the config/kohana.php file.
+ *
+ * @see  http://docs.kohanaphp.com/install#application
+ */
+$application = get_option('kohana_application_path');
+
+/**
+ * The directory in which your modules are located.
+ *
+ * @see  http://docs.kohanaphp.com/install#modules
+ */
+$modules = get_option('kohana_module_path');
+
+/**
+ * The directory in which the Kohana resources are located. The system
+ * directory must contain the classes/kohana.php file.
+ *
+ * @see  http://docs.kohanaphp.com/install#system
+ */
+$system = get_option('kohana_system_path');
+
+/**
+ * The default extension of resource files. If you change this, all resources
+ * must be renamed to use the new extension.
+ *
+ * @see  http://docs.kohanaphp.com/install#ext
+ */
+define('EXT', get_option('kohana_ext') );
+
+/**
+ * Set the PHP error reporting level. If you set this in php.ini, you remove this.
+ * @see  http://php.net/error_reporting
+ *
+ * When developing your application, it is highly recommended to enable notices
+ * and strict warnings. Enable them by using: E_ALL | E_STRICT
+ *
+ * In a production environment, it is safe to ignore notices and strict warnings.
+ * Disable them by using: E_ALL ^ E_NOTICE
+ */
+// error_reporting(E_ALL | E_STRICT);
+
+/**
+ * End of standard configuration! Changing any of the code below should only be
+ * attempted by those with a working knowledge of Kohana internals.
+ *
+ * @see  http://docs.kohanaphp.com/bootstrap
+ */
+
+// Set the full path to the docroot
+define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+
+// Make the application relative to the docroot
+if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
+	$application = DOCROOT.$application;
+
+// Make the modules relative to the docroot
+if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
+	$modules = DOCROOT.$modules;
+
+// Make the system relative to the docroot
+if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
+	$system = DOCROOT.$system;
+
+// Define the absolute paths for configured directories
+define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
+define('MODPATH', realpath($modules).DIRECTORY_SEPARATOR);
+define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
+
+// Clean up the configuration vars
+unset($application, $modules, $system);
+
+// Define the start time of the application
+define('KOHANA_START_TIME', microtime(TRUE));
+
+// Load the base, low-level functions	***** Not including base.php 
+// require SYSPATH.'base'.EXT;
+
+// Load the main Kohana class			***** Include Kohana class from path defined in kohana settings
+// require SYSPATH.'classes/kohana'.EXT;
+require get_option('kohana_system_path').'classes/kohana'.get_option('kohana_ext');
+
+// Bootstrap the application			***** Not including bootstrap in application path.
+// require APPPATH.'bootstrap'.EXT;
